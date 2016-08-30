def009fe1f942dc02d3e2d19be46573fd20fd3d6
Anri Asaturov <anri82@gmail.com>
2016-08-30 23:58:41 +0300

    test(crypto): add secretbox compatibility cases

--- test/crypto/secret.test.js
+++ test/crypto/secret.test.js
@@ -2,6 +2,8 @@
 //  Secret key encryption module testing
 //
 const crypto = require('../../src/crypto/secret');
+const nacl = require('tweetnacl');
+const util = require('../../src/crypto/util');
 
 describe('Secret key encryption module', () => {
     it('should encrypt and decrypt', () => {
@@ -11,5 +13,26 @@ describe('Secret key encryption module', () => {
         const actual = crypto.decrypt(crypto.encrypt(message, key), key);
         actual.should.eql(message);
     });
-    // TODO compatibility with original nacl.secretbox() and nacl.secretbox.open()
+
+    it('should be compatible with secretbox: new encrypt - old decrypt', () => {
+        const key = new Uint8Array([127, 216, 168, 148, 177, 189, 134, 245, 107, 28, 100, 181, 50, 32, 94,
+                                    149, 237, 193, 148, 19, 11, 103, 73, 45, 1, 17, 102, 222, 82, 227, 123, 157]);
+        const message = new Uint8Array([1, 2, 3, 4, 5]);
+        const newFormatEncrypted = crypto.encrypt(message, key);
+        const nonce = newFormatEncrypted.subarray(-24);
+        const box = newFormatEncrypted.subarray(16, -24);
+        const decrypted = nacl.secretbox.open(box, nonce, key);
+        decrypted.should.eql(message);
+    });
+
+    it('should be compatible with secretbox: old encrypt - new decrypt', () => {
+        const key = new Uint8Array([127, 216, 168, 148, 177, 189, 134, 245, 107, 28, 100, 181, 50, 32, 94,
+                                    149, 237, 193, 148, 19, 11, 103, 73, 45, 1, 17, 102, 222, 82, 227, 123, 157]);
+        const message = new Uint8Array([1, 2, 3, 4, 5]);
+        const nonce = util.getRandomNonce();
+        const box = nacl.secretbox(message, nonce, key);
+        const oldFormatEncrypted = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ...box, ...nonce]);
+        const decrypted = crypto.decrypt(oldFormatEncrypted, key);
+        decrypted.should.eql(message);
+    });
 });
