43d58cb792d237f1d49d855602be1b07ee8f5155
Anri Asaturov <anri82@gmail.com>
2016-09-29 14:17:55 +0300

    WIP: networking, server errors

--- src/errors.js
+++ src/errors.js
@@ -27,7 +27,7 @@ CustomError.prototype.constructor = CustomError;
  * then call normalize and pass the result you've got together with fallback message
  * that will be wrapped in Error object and returned in case the result wasn't instance of Error
  */
-exports.normalize = function(unknownErrorObject: any, failoverMessage: string): Error {
+module.exports.normalize = function(unknownErrorObject: any, failoverMessage: string): Error {
     if (unknownErrorObject) {
         if (unknownErrorObject instanceof Error) {
             return unknownErrorObject;
@@ -38,3 +38,41 @@ exports.normalize = function(unknownErrorObject: any, failoverMessage: string): 
     }
     return new Error(failoverMessage);
 };
+
+// -- Server Errors ----------------------------------------------------------------------------------------------------
+const serverErrorCodes = {
+    notFound: 404,
+    malformedRequest: 406,
+    apiVersionError: 410,
+    incorrectPublicKey: 411,
+    quotaExceeded: 413,
+    authError: 423,
+    twoFAAuthRequired: 424,
+    accountThrottled: 425,
+    accountBlacklisted: 426,
+    captchaPending: 435,
+    accountClosed: 488,
+    incorrectTimestamp: 442,
+    genericServerError: 400
+};
+// reverse map
+const serverErrorMap = {};
+Object.keys(serverErrorCodes).forEach((key: string) => {
+    serverErrorMap[serverErrorCodes[key]] = key;
+});
+
+function ServerError(code: number) {
+    const type = serverErrorMap[code] || 'Unknown server error';
+    this.message = type;
+    const error = Error.call(this, this.message);
+    this.name = `ServerError: ${code}: ${type}`;
+    this.code = code;
+    this.stack = error.stack;
+}
+
+ServerError.prototype = Object.create(Error.prototype);
+ServerError.prototype.constructor = ServerError;
+ServerError.codes = serverErrorCodes;
+
+module.exports.ServerError = ServerError;
+//----------------------------------------------------------------------------------------------------------------------
