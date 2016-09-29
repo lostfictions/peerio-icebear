7d0c84edf5cdb82bb94cef4efeec2e413808af39
Anri Asaturov <anri82@gmail.com>
2016-09-29 14:16:37 +0300

    test(helpers): debug helper for modules

---
+++ test/debug-modules.js
@@ -0,0 +1,5 @@
+// Just a list of modules pre-required to access from console
+
+const m = window.modules = {};
+
+m.socket = require('../src/network/socket');
--- test/test-index.js
+++ test/test-index.js
@@ -2,3 +2,5 @@
 // current directory and all subdirectories
 const testsContext = require.context('.', true, /\.test$/);
 testsContext.keys().forEach(testsContext);
+
+require('./debug-modules');
