d89599b8865beeb570e23917ba499d7103eb5ae8
Anri Asaturov <anri82@gmail.com>
2016-11-04 01:32:21 +0200

    WIP: messaging. Contact model rework

--- src/models/contact.js
+++ src/models/contact.js
@@ -1,60 +1,41 @@
 const socket = require('../network/socket');
-/*
+const { observable, action } = require('mobx');
+
 class Contact {
+    @observable loading = false;
+    @observable notFound = false;
+    @observable firstName = '';
+    @observable lastName = '';
+    @observable encryptionPublicKey = '';
+    @observable signingPublicKey = '';
 
     constructor(username) {
         this.username = username;
-        this.found = null; // true - contact data was filled from server,
-                           // false - not found
-                           // null - not requested yet
-        this.load = this.load.bind(this);
+        this._load();
     }
 
-    load() {
-        return socket.send('/auth/lookupUser', { string: this.username })
-            .then(resp => {
+    _load() {
+        this.loading = true;
+        socket.send('/auth/lookupUser', { string: this.username })
+            .then(action(resp => {
+                // currently there are old users in the system that don't have encryption public keys
                 if (!resp || !resp.length || !resp[0].profile.encryptionPublicKey) {
                     this.found = false;
                     return Promise.resolve(this);
                 }
-                this.encryptionPublicKey = new Uint8Array(resp[0].profile.encryptionPublicKey);
-                this.found = true;
+                const profile = resp[0].profile;
+
+                this.encryptionPublicKey = new Uint8Array(profile.encryptionPublicKey);
+                this.signingPublicKey = new Uint8Array(profile.signingPublicKey);
+                this.firstName = profile.firstName || '';
+                this.lastName = profile.lastName || '';
                 return this;
-            })
+            }))
             .catch(err => {
                 console.error(err);
-                this.found = false;
+                this.notFound = true;
                 return this;
             });
-    }
-
-
-}
-
-module.exports = Contact;
-
-*/
-const { observable, action } = require('mobx');
-
-let count = 0;
-
-class Contact {
-    @observable loading=false;
-    @observable notFound= false;
-    @observable firstName ='';
-    @observable lastName = '';
-
-    constructor(username) {
-        this.username = username;
-    }
-
-    load() {
-        this.loading = true;
-        setTimeout(action(() => {
-            this.firstName = `First${count++}`;
-            this.lastName = `First${count++}`;
-            this.loading = false;
-        }), Math.random() * 1500);
         return this;
     }
 }
