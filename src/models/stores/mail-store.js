5d4c8a591fa88ab66ed176c455424031c1bbe939
FHV <floh@flohdot.org>
2017-01-26 19:38:10 -0500

    fix: remove unused fs

--- src/models/stores/mail-store.js
+++ src/models/stores/mail-store.js
@@ -1,5 +1,4 @@
 const _ = require('lodash');
-const fs = require('fs');
 const { observable, action, computed } = require('mobx');
 const socket = require('../../network/socket');
 const Ghost = require('../kegs/ghost');
