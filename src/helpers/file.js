b8db9cb96f06577336599b237cf3e99c16c1ad7a
Samvel Avanesov <seavan@gmail.com>
2017-02-24 18:13:01 +0300

    feat(lib): file name utilities

--- src/helpers/file.js
+++ src/helpers/file.js
@@ -2,10 +2,14 @@ function getFileName(path) {
     return path.replace(/^.*[\\/]/, '');
 }
 
+function getFileNameWithoutExtension(path) {
+    return getFileName(path).replace(/\.\w+$/, '');
+}
+
 function getFileExtension(name) {
     let extension = name.toLowerCase().match(/\.\w+$/);
     extension = extension ? extension[0].substring(1) : '';
     return extension;
 }
 
-module.exports = { getFileName, getFileExtension };
+module.exports = { getFileName, getFileExtension, getFileNameWithoutExtension };
