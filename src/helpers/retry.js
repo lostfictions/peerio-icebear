a566780d6b5d3114f03e9cb789f9a5478f7c1a28
Anri Asaturov <anri82@gmail.com>
2017-04-03 03:44:10 +0300

    feat(retry): failsafe retry with increasing interval and retry count limit

--- src/helpers/retry.js
+++ src/helpers/retry.js
@@ -1,7 +1,12 @@
 const socket = require('../network/socket');
+const errors = require('../errors');
+
+const maxRetryCount = 120; // will bail out after this amount of retries
+const minRetryInterval = 1000; // will start with this interval between retries
+const maxRetryInterval = 10000; // this will be the maximum interval between retries
+const retryIntervalMultFactor = 250; // every retry will add this amount to retry interval
 
 const callsInProgress = {};
-
 /**
  * 1. Executes the passed function
  * 2. If promise returend by function rejects - goto 1
@@ -15,15 +20,17 @@ function retryUntilSuccess(fn, id = Math.random(), thisIsRetry) {
     if (!thisIsRetry && callsInProgress[id]) return callsInProgress[id].promise;
 
     const callInfo = { retryCount: 0 };
-    callInfo.promise = new Promise(resolve => {
+    callInfo.promise = new Promise((resolve, reject) => {
         callInfo.resolve = resolve;
+        callInfo.reject = reject;
     });
     callsInProgress[id] = callInfo;
 
     fn().tap(() => {
-        callsInProgress[id].resolve();
+        callInfo.resolve();
         delete callsInProgress[id];
     }).catch(err => {
+        callInfo.lastError = err;
         scheduleRetry(fn, id);
         console.debug(err);
     });
@@ -33,9 +40,18 @@ function retryUntilSuccess(fn, id = Math.random(), thisIsRetry) {
 
 function scheduleRetry(fn, id) {
     console.debug(`Retrying ${id} in 1 second`);
+    const callInfo = callsInProgress[id];
     // todo: detect 2fa and pause retries
-    callsInProgress[id].retryCount++;
-    setTimeout(() => socket.onceAuthenticated(() => retryUntilSuccess(fn, id, true)), 1000);
+    if (callInfo.retryCount++ > maxRetryCount) {
+        console.error(`Maximum retry count reached for action id ${id}. Giving up, rejecting promise.`);
+        console.debug(fn);
+        callInfo.reject(errors.normalize(callInfo.lastError));
+        return;
+    }
+    setTimeout(
+        () => socket.onceAuthenticated(() => retryUntilSuccess(fn, id, true)),
+        minRetryInterval + Math.min(maxRetryInterval, (callInfo.retryCount * retryIntervalMultFactor))
+    );
 }
 
 
