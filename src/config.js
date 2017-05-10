538558327aa9eacea3609982b014ccdaf2cf2f5c
Anri Asaturov <anri82@gmail.com>
2017-05-11 01:50:51 +0300

    feat(chat): tweak page size for more comfortable chat experience

--- src/config.js
+++ src/config.js
@@ -73,8 +73,8 @@ const config = new class {
     chat = {
         maxInitalChats: 15, // maximum amount of chats to load initially (favorite chats will load in full)
         initialPageSize: 40, // amount of messages to load to a newly opened chat
-        pageSize: 20, // when next/prev pages is requested, chat will load this amount of messages
-        maxLoadedMessages: 60, // chat will remove excess of messages if paging resulted in larger count
+        pageSize: 30, // when next/prev pages is requested, chat will load this amount of messages
+        maxLoadedMessages: 80, // chat will remove excess of messages if paging resulted in larger count
         decryptQueueThrottle: 0 // delay between decryption of individual messages when processing a batch
     };
 }();
