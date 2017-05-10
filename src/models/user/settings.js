b57f5ff9fae405086937c0325e6ea607a64aac42
FHV <floh@flohdot.org>
2017-05-09 22:02:24 -0400

    WIP: user preferences

--- src/models/user/settings.js
+++ src/models/user/settings.js
@@ -5,6 +5,7 @@ class Settings extends Keg {
     @observable contactNotifications = false;
     @observable contactRequestNotifications = false;
     @observable messageNotifications = false;
+    @observable errorTracking = false;
 
     constructor(user) {
         super('settings', 'settings', user.kegDb, true);
@@ -15,7 +16,8 @@ class Settings extends Keg {
         return {
             contactNotifications: this.contactNotifications,
             contactRequestNotifications: this.contactRequestNotifications,
-            messageNotifications: this.messageNotifications
+            messageNotifications: this.messageNotifications,
+            errorTracking: this.errorTracking
         };
     }
 
@@ -23,6 +25,7 @@ class Settings extends Keg {
         this.contactNotifications = data.contactNotifications;
         this.contactRequestNotifications = data.contactRequestNotifications;
         this.messageNotifications = data.messageNotifications;
+        this.errorTracking = data.errorTracking;
         this.loaded = true;
     }
 
