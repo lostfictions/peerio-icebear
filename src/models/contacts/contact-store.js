59ecafe46eae04ba9c4413c1b3e7c5690ecac4dd
Anri Asaturov <anri82@gmail.com>
2017-05-18 19:19:30 +0300

    feat(contacts): contactStore.filter(token, [collection]) smart user search

--- src/models/contacts/contact-store.js
+++ src/models/contacts/contact-store.js
@@ -69,6 +69,32 @@ class ContactStore {
             res.kegs.forEach(keg => this.getContact(keg.props.username));
         });
     }
+    /**
+     * Filters contacts by username and First Last name based on passed token
+     * @param {string} token - search query string
+     * @param {Array<Contact>} list - optional list to search in, by default it will search in contact store
+     */
+    filter(token, list) {
+        token = token.toLocaleLowerCase(); // eslint-disable-line
+        let removeUnavailable = false;
+        if (!list) {
+            list = this.contacts; // eslint-disable-line
+            removeUnavailable = true;
+        }
+        return list
+            .filter((c) => {
+                if (removeUnavailable) {
+                    if (c.loading || c.notFound) return false;
+                }
+                return c.username.includes(token) || c.fullNameLower.includes(token);
+            }).sort((c1, c2) => {
+                if (c1.username.startsWith(token)) return -1;
+                if (c2.username.startsWith(token)) return 1;
+                if (c1.fullNameLower.startsWith(token)) return -1;
+                if (c2.fullNameLower.startsWith(token)) return 1;
+                return 0;
+            });
+    }
 }
 
 const store = new ContactStore();
