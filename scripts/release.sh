e8e4946e0909a2aa1c39245d9794f888215b199f
Anri Asaturov <anri82@gmail.com>
2017-06-29 15:14:54 +0300

    chore: release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,17 +1,34 @@
 set -e
 export NODE_ENV=production
 
+
+# offer to switch to master
 branch="$(git rev-parse --abbrev-ref HEAD)"
 if [ branch != "master" ]; then
-    echo "---------------------------------------------------------------------------------------"
-    echo "Can't release from $branch branch. Switch to master and merge latest release branch to it.";
-    echo "---------------------------------------------------------------------------------------"
-    exit;
+    git status -uall
+    read -p "Can't release from $branch branch. Switch to master? (y/n)" choice
+    case "$choice" in
+    y|Y ) echo "yes";;
+    n|N ) exit;;
+    * ) exit;;
+    esac
+
+    git checkout master
 fi
 
+
+read -p "Merge dev => master? (y/n)" choice
+case "$choice" in
+  y|Y ) git merge dev;;
+  n|N ) exit;;
+  * ) exit;;
+esac
+
+# check the state of the branch before anythig
 npm run validate
 
-read -p "Did you merge to master everything that was needed? Confirm release? (y/n)" choice
+# last confirmation before release build
+read -p "All the changes needed are in master? No errors? Confirm release? (y/n)" choice
 case "$choice" in
   y|Y ) echo "yes";;
   n|N ) exit;;
@@ -32,6 +49,7 @@ echo "---------------------------"
 
 standard-version
 
+# last confirmation before release push
 read -p "All good? Push to git and publish to npm? (y/n)" choice
 case "$choice" in
   y|Y ) echo "yes";;
@@ -40,6 +58,17 @@ case "$choice" in
 esac
 
 git push --follow-tags origin master && npm publish
+
+# no reason to stay in master
+git checkout dev
+
+# megre changes back to dev?
+read -p "Merge master => dev? (y/n)" choice
+case "$choice" in
+  y|Y ) git merge master;;
+  n|N ) ;;
+  * ) ;;
+esac
 
 echo '3. Opening peerio-icebear github, go edit release notes publish the release.'
 echo '---------------------------------------------------------'
