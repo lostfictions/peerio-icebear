44c14927aa40556d92a81a0616544c8ebf55dc5b
Alexandra Marin <alexandra.marin01@gmail.com>
2017-09-26 17:56:48 +0300

    WIP: add websocket for tests

--- test/e2e/config.js
+++ test/e2e/config.js
@@ -4,6 +4,12 @@ function getNewAppInstance() {
     const cfg = require('../../src/config');
     const FileStream = require('../../src/models/files/node-file-stream');
     const StorageEngine = require('../../src/models/storage/node-json-storage');
+    
+    const W3CWebSocket = require('websocket').client;
+    
+    global.WebSocket = W3CWebSocket;
+
+
 
     cfg.appVersion = '';
     cfg.platform = 'electron'; // todo: change name
@@ -12,8 +18,12 @@ function getNewAppInstance() {
     cfg.FileStream = FileStream;
     cfg.StorageEngine = StorageEngine;
     cfg.StorageEngine.storageFolder = path.join(os.homedir(), '.peerio-icebear-tests');
+    cfg.socketServerUrl = 'wss://hocuspocus.peerio.com';
 
     const icebear = require('../../src/index');
+
+    icebear.socket.start();
+
     return icebear;
 }
 
--- test/global-setup.js
+++ test/global-setup.js
@@ -2,6 +2,7 @@
 const chai = require('chai');
 const sinonChai = require('sinon-chai');
 const chaiAsPromised = require('chai-as-promised');
+const W3CWebSocket = require('websocket').w3cwebsocket;
 
 chai.should();
 chai.use(sinonChai);
@@ -11,4 +12,5 @@ if (!console.debug) {
     console.debug = console.log.bind(console);
 }
 
+global.WebSocket = W3CWebSocket;
 global.expect = chai.expect;
