db0963586f461add99d940310437144a29173da3
Alexandra Marin <alexandra.marin01@gmail.com>
2017-10-09 14:06:39 +0300

    WIP: add test error for more than 3 rooms created

--- test/e2e/rooms/supportCode/rooms.js
+++ test/e2e/rooms/supportCode/rooms.js
@@ -5,7 +5,8 @@ const { when } = require('mobx');
 defineSupportCode(({ Before, Given, Then, When }) => {
     let app;
     const roomName = 'test-room';
-    let invitedUser;
+    const roomPurpose = 'test-room';
+    const invitedUserId = 'do38j9ncwji7frf48g4jew9vd3f17p';
     let chat;
 
     Before((testCase, done) => {
@@ -15,9 +16,10 @@ defineSupportCode(({ Before, Given, Then, When }) => {
 
     // Scenario: Create room
     When('I create a room', () => {
-        invitedUser = new app.Contact('do38j9ncwji7frf48g4jew9vd3f17p');
+        const invited = new app.Contact(invitedUserId, {}, true);
+        console.log(app.User.current.channelsLeft); 
         
-        chat = app.chatStore.startChat(invitedUser, true, roomName, 'test-purpose');
+        chat = app.chatStore.startChat([invited], true, roomName, roomPurpose);
         console.log(chat); // TypeError: Cannot read property 'added' of null
     });
 
@@ -27,7 +29,7 @@ defineSupportCode(({ Before, Given, Then, When }) => {
 
     // Scenario: Delete room
     Given('I am an admin of {a room}', (callback) => {
-        chat = app.chatStore.startChat(invitedUser, true, roomName, 'test-purpose');
+        chat = app.chatStore.startChat(invitedUserId, true, roomName, 'test-purpose');
         when(() => chat.added === true, () => {
             // console.log('can i admin '+chat.canIAdmin);
             callback(null, 'pending');
@@ -41,5 +43,14 @@ defineSupportCode(({ Before, Given, Then, When }) => {
 
     Then('nobody should be able to access {a room}', (callback) => {
         callback(null, 'pending');
+    });
+
+    // Scenario: Can not create more than 3 rooms
+    When('I created 3 rooms', () => {
+        const invited = new app.Contact(invitedUserId, {}, true);
+        console.log(app.User.current.channelsLeft); // 0
+
+        chat = app.chatStore.startChat([invited], true, roomName, roomPurpose);
+        console.log(chat); // TypeError: Cannot read property 'added' of null
     });
 });
