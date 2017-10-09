4c8a97d5a42b8f26de74e6510f75a0ae92e5667b
Alexandra Marin <alexandra.marin01@gmail.com>
2017-10-09 13:41:57 +0300

    WIP: delete room

--- test/e2e/rooms/supportCode/rooms.js
+++ test/e2e/rooms/supportCode/rooms.js
@@ -10,16 +10,36 @@ defineSupportCode(({ Before, Given, Then, When }) => {
 
     Before((testCase, done) => {
         app = getNewAppInstance();
-        // invitedUser = new app.Contact('do38j9ncwji7frf48g4jew9vd3f17p');
         when(() => app.socket.connected, done);
     });
 
     // Scenario: Create room
     When('I create a room', () => {
+        invitedUser = new app.Contact('do38j9ncwji7frf48g4jew9vd3f17p');
+        
         chat = app.chatStore.startChat(invitedUser, true, roomName, 'test-purpose');
+        console.log(chat); // TypeError: Cannot read property 'added' of null
     });
 
     Then('I can enter the room', (done) => {
         when(() => chat.added === true, done);
+    });
+
+    // Scenario: Delete room
+    Given('I am an admin of {a room}', (callback) => {
+        chat = app.chatStore.startChat(invitedUser, true, roomName, 'test-purpose');
+        when(() => chat.added === true, () => {
+            // console.log('can i admin '+chat.canIAdmin);
+            callback(null, 'pending');
+        });
+    });
+
+    When('I delete {a room}', (callback) => {
+        chat.delete();
+        callback(null, 'pending');
+    });
+
+    Then('nobody should be able to access {a room}', (callback) => {
+        callback(null, 'pending');
     });
 });
