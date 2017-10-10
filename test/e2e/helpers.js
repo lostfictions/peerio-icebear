22ec9b9b6a5aeb816fade01214e5e44e7dc0fa3e
Alexandra Marin <alexandra.marin01@gmail.com>
2017-10-10 16:18:27 +0300

    refactor: break email confirmation into more helpers

--- test/e2e/helpers.js
+++ test/e2e/helpers.js
@@ -2,9 +2,15 @@ const mailinator = require('mailinator-api');
 const http = require('http');
 const https = require('https');
 
-const usernameChars = '0123456789abcdefghijklmnopqrstuvwxyz';
+const getConfirmationEmailAddress = (body) => {
+    const regex = /https:\\\/\\\/hocuspocus.peerio.com\\\/confirm-address\\\/\w*/ig;
+    let found = body.match(regex)[0];
+    found = found.replace(/\\\//g, '/');
 
-// generates 16-character random usernames
+    return found;
+};
+
+const usernameChars = '0123456789abcdefghijklmnopqrstuvwxyz';
 const getRandomUsername = () => {
     let username = '';
     for (let i = 0; i < 30; i++) {
@@ -19,38 +25,24 @@ const confirmUserEmail = (user, done) => {
         process.env.MAILINATOR_KEY,
         (reply) => {
             const mailbox = JSON.parse(reply);
-            const confirmationEmail = mailbox.messages
+            const confirmationEmailId = mailbox.messages
                 .find(x => x.subject.includes(user))
                 .id;
 
-            console.log('email is:', confirmationEmail);
-
-            const emailUrl = `http://api.mailinator.com/api/email?id=${confirmationEmail}&token=${process.env.MAILINATOR_KEY}`;
+            const emailUrl = `http://api.mailinator.com/api/email?id=${confirmationEmailId}&token=${process.env.MAILINATOR_KEY}`;
 
             let body = '';
-            http
-                .get(emailUrl, (res) => {
-                    res.on('data', (chunk) => {
-                        body += chunk;
+            http.get(emailUrl, (res) => {
+                res.on('data', (chunk) => { body += chunk; });
+                res.on('error', (e) => done(e.message, 'failed'));
+                res.on('end', () => {
+                    const url = getConfirmationEmailAddress(body);
+                    https.get(url, (resp) => {
+                        resp.on('error', (e) => done(e.message, 'failed'));
+                        resp.on('end', () => done());
                     });
-                    res.on('end', () => {
-                        const regex = /https:\\\/\\\/hocuspocus.peerio.com\\\/confirm-address\\\/\w*/ig;
-
-                        let found = body.match(regex)[0];
-                        found = found.replace(/\\\//g, '/');
-                        console.log(found);
-
-                        https
-                            .get(found, (resp) => {
-                                resp.on('data', (chunk) => {
-                                    console.log(chunk.toString());
-                                });
-                                resp.on('end', () => done());
-                                resp.on('error', (e) => console.log(e.message));
-                            });
-                    });
-                })
-                .on('error', (e) => done(e.message, 'failed'));
+                });
+            });
         });
 };
 
