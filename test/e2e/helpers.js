79e58a2aa93d62d29ecf8734017ef454afa5e36a
Alexandra Marin <alexandra.marin01@gmail.com>
2017-10-11 13:17:07 +0300

    fix: remove mailinator package from dependencies b/c causing errors in returned response

--- test/e2e/helpers.js
+++ test/e2e/helpers.js
@@ -1,8 +1,8 @@
-const mailinator = require('mailinator-api');
 const http = require('http');
 const https = require('https');
 
 const confirmLabel = 'Confirm your account';
+const newEmailLabel = 'confirm your new address';
 
 const getConfirmationEmailAddress = (body) => {
     const regex = /https:\\\/\\\/hocuspocus.peerio.com\\\/confirm-address\\\/\w*/ig;
@@ -22,31 +22,36 @@ const getRandomUsername = () => {
 };
 
 const confirmUserEmail = (user, err, done) => {
-    mailinator.getMailinatorInboxJSON(
-        process.env.PEERIO_ADMIN_EMAIL,
-        process.env.MAILINATOR_KEY,
-        (reply) => {
-            const mailbox = JSON.parse(reply);
+    const inbox = `http://api.mailinator.com/api/inbox?to=${process.env.PEERIO_ADMIN_EMAIL}&token=${process.env.MAILINATOR_KEY}`;
+    http.get(inbox, (response) => {
+        let emails = '';
+        response.on('data', (chunk) => { emails += chunk; });
+        response.on('error', (e) => err(e.message));
+        response.on('end', () => {
+            const mailbox = JSON.parse(emails);
             const confirmationEmailId = mailbox.messages
-                .find(x => x.subject.includes(user) && x.subject.includes(confirmLabel))
+                .find(x => x.subject.includes(user) &&
+                    (x.subject.includes(confirmLabel) ||
+                        x.subject.includes(newEmailLabel)))
                 .id;
 
             const emailUrl = `http://api.mailinator.com/api/email?id=${confirmationEmailId}&token=${process.env.MAILINATOR_KEY}`;
 
-            let body = '';
             http.get(emailUrl, (res) => {
+                let body = '';
                 res.on('data', (chunk) => { body += chunk; });
                 res.on('error', (e) => err(e.message));
                 res.on('end', () => {
                     const url = getConfirmationEmailAddress(body);
                     https.get(url, (resp) => {
-                        resp.on('data', () => {});
+                        resp.on('data', () => { });
                         resp.on('error', (e) => err(e.message));
                         resp.on('end', () => done());
                     });
                 });
             });
         });
+    });
 };
 
 module.exports = { getRandomUsername, confirmUserEmail };
