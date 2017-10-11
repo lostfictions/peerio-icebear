ac1b60f59685c378b171acad081d114e551b5d61
Alexandra Marin <alexandra.marin01@gmail.com>
2017-10-11 14:31:10 +0300

    refactor: promisify email helper and break into smaller pieces

--- test/e2e/helpers.js
+++ test/e2e/helpers.js
@@ -4,14 +4,6 @@ const https = require('https');
 const confirmLabel = 'Confirm your account';
 const newEmailLabel = 'confirm your new address';
 
-const getConfirmationEmailAddress = (body) => {
-    const regex = /https:\\\/\\\/hocuspocus.peerio.com\\\/confirm-address\\\/\w*/ig;
-    let found = body.match(regex)[0];
-    found = found.replace(/\\\//g, '/');
-
-    return found;
-};
-
 const usernameChars = '0123456789abcdefghijklmnopqrstuvwxyz';
 const getRandomUsername = () => {
     let username = '';
@@ -21,37 +13,82 @@ const getRandomUsername = () => {
     return username;
 };
 
-const confirmUserEmail = (user, err, done) => {
-    const inbox = `http://api.mailinator.com/api/inbox?to=${process.env.PEERIO_ADMIN_EMAIL}&token=${process.env.MAILINATOR_KEY}`;
-    http.get(inbox, (response) => {
-        let emails = '';
-        response.on('data', (chunk) => { emails += chunk; });
-        response.on('error', (e) => err(e.message));
-        response.on('end', () => {
-            const mailbox = JSON.parse(emails);
-            const confirmationEmailId = mailbox.messages
-                .find(x => x.subject.includes(user) &&
-                    (x.subject.includes(confirmLabel) ||
-                        x.subject.includes(newEmailLabel)))
-                .id;
-
-            const emailUrl = `http://api.mailinator.com/api/email?id=${confirmationEmailId}&token=${process.env.MAILINATOR_KEY}`;
-
-            http.get(emailUrl, (res) => {
-                let body = '';
-                res.on('data', (chunk) => { body += chunk; });
-                res.on('error', (e) => err(e.message));
-                res.on('end', () => {
-                    const url = getConfirmationEmailAddress(body);
-                    https.get(url, (resp) => {
-                        resp.on('data', () => { });
-                        resp.on('error', (e) => err(e.message));
-                        resp.on('end', () => done());
-                    });
-                });
+const getMailbox = (user) => {
+    return new Promise((resolve, reject) => {
+        const inbox = `http://api.mailinator.com/api/inbox?to=${process.env.PEERIO_ADMIN_EMAIL}&token=${process.env.MAILINATOR_KEY}`;
+        http.get(inbox, (response) => {
+            let emails = '';
+            response.on('data', (chunk) => { emails += chunk; });
+            response.on('error', (e) => console.log(e.message));
+            response.on('end', () => {
+                resolve(emails);
             });
         });
     });
 };
 
+
+const addressedToUser = (email, user) => {
+    const containsName = email.subject.includes(user);
+    const containsText = email.subject.includes(confirmLabel) || email.subject.includes(newEmailLabel);
+
+    return containsName && containsText;
+};
+
+const getConfirmationEmailId = (user, emails) => {
+    const mailbox = JSON.parse(emails);
+    const confirmationEmailId = mailbox.messages
+        .find(email => addressedToUser(email, user))
+        .id;
+
+    return confirmationEmailId;
+};
+
+const getConfirmationLink = (body) => {
+    const regex = /https:\\\/\\\/hocuspocus.peerio.com\\\/confirm-address\\\/\w*/ig;
+    let found = body.match(regex)[0];
+    found = found.replace(/\\\//g, '/');
+
+    return found;
+};
+
+const openConfirmationEmail = (confirmationEmailId) => {
+    return new Promise((resolve, reject) => {
+        const emailUrl = `http://api.mailinator.com/api/email?id=${confirmationEmailId}&token=${process.env.MAILINATOR_KEY}`;
+
+        http.get(emailUrl, (res) => {
+            let body = '';
+            res.on('data', (chunk) => { body += chunk; });
+            res.on('error', (e) => console.log(e.message));
+            res.on('end', () => {
+                const url = getConfirmationLink(body);
+                resolve(url);
+            });
+        });
+    });
+};
+
+const openConfirmationLink = (url) => {
+    return new Promise((resolve, reject) => {
+        https.get(url, (resp) => {
+            resp.on('data', () => { });
+            resp.on('error', (e) => console.log(e.message));
+            resp.on('end', () => resolve());
+        });
+    });
+};
+
+const confirmUserEmail = (user, err, done) => {
+    getMailbox()
+        .then(emails => {
+            const id = getConfirmationEmailId(user, emails);
+            openConfirmationEmail(id)
+                .then(url => {
+                    openConfirmationLink(url)
+                        .then(done);
+                });
+        });
+};
+
 module.exports = { getRandomUsername, confirmUserEmail };
+
