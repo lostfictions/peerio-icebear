372cb8b2c904616af3829dbb7ebbd2602005daf4
Alexandra Marin <alexandra.marin01@gmail.com>
2017-10-27 18:08:04 +0300

    refactor: remove unused things for unit tests from global setup

--- test/global-setup.js
+++ test/global-setup.js
@@ -1,8 +1,6 @@
 const chai = require('chai');
 const sinonChai = require('sinon-chai');
 const chaiAsPromised = require('chai-as-promised');
-const W3CWebSocket = require('websocket').w3cwebsocket;
-const XMLHttpRequest = require('w3c-xmlhttprequest').XMLHttpRequest;
 
 chai.should();
 chai.use(sinonChai);
@@ -12,6 +10,4 @@ if (!console.debug) {
     console.debug = console.log.bind(console);
 }
 
-global.XMLHttpRequest = XMLHttpRequest;
-global.WebSocket = W3CWebSocket;
 global.expect = chai.expect;
