3f2266519eb4e01874c41b423f37f43588dbf5e3
Anri Asaturov <anri82@gmail.com>
2017-10-31 04:28:09 +0200

    chore: ci

--- scripts/dev-release.sh
+++ scripts/dev-release.sh
@@ -1,7 +1,9 @@
-# this script is supposed to run from ci
+# this script is supposed to run from ci only
 set -e
 
 branch="$(git rev-parse --abbrev-ref HEAD)"
+git config user.email "anri82@gmail.com"
+git config user.name "Anri Asaturov"
 echo branch $branch
 if [ $branch == "dev" ]; then
     echo "releasing dev version"
