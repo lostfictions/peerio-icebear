a57f547282951d381a4f83f2882002b35364591a
Anri Asaturov <anri82@gmail.com>
2017-10-31 17:06:56 +0200

    chore: fix ci deploy

--- scripts/dev-release.sh
+++ scripts/dev-release.sh
@@ -2,7 +2,7 @@
 set -e
 
 branch="$(git rev-parse --abbrev-ref HEAD)"
-git config user.email "anri82@gmail.com"
+git config user.email "anri82+icebeardeploy@gmail.com"
 git config user.name "Anri Asaturov"
 echo branch $branch
 if [ $branch == "dev" ]; then
