b4af55e0406df909766db99db59ff5905eb5b2d3
Anri Asaturov <anri82@gmail.com>
2017-11-13 07:14:32 +0200

    fix: hooks [skip ci]

--- scripts/npm_install_if_changed.js
+++ scripts/npm_install_if_changed.js
@@ -2,9 +2,14 @@ const fs = require('fs');
 const { execSync } = require('child_process');
 
 try {
-    const existing = fs.readFileSync('.package.json.md5');
+    let existing;
+    try {
+        existing = fs.readFileSync('.package.json.md5');
+    } catch (ex) {
+        // don't care
+    }
     const actual = execSync(process.platform === 'darwin' ? 'md5 -q package.json' : 'md5sum package.json');
-    if (existing.toString !== actual.toString) {
+    if (existing.toString() !== actual.toString()) {
         console.log('package.json has changed, running npm install.');
         execSync('npm install');
     } else {
