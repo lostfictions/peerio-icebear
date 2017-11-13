27ff93b0fd620d21ab1b98b4e1944edd9c5ac1c8
Anri Asaturov <anri82@gmail.com>
2017-11-13 05:21:42 +0200

    chore: run npm install hook only if package.json has changed

---
+++ scripts/npm_install_if_changed.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const { execSync } = require('child_process');
+
+try {
+    const existing = fs.readFileSync('.package.json.md5');
+    const actual = execSync('md5 -q package.json');
+    if (existing.toString !== actual.toString) {
+        console.log('package.json has changed, running npm install.');
+        execSync('npm install');
+    } else {
+        console.log('package.json has not changed, skipping npm install.');
+    }
+} catch (ex) {
+    console.error(ex);
+}
