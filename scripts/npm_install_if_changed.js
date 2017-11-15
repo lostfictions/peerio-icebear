47a8d3a26ccf0845036f1f9f06ccce07ad5be853
Anri Asaturov <anri82@gmail.com>
2017-11-15 05:35:48 +0200

    chore: fix hooks [skip ci]

--- scripts/npm_install_if_changed.js
+++ scripts/npm_install_if_changed.js
@@ -1,17 +1,19 @@
 const fs = require('fs');
-const { execSync } = require('child_process');
+const { execSync, spawnSync } = require('child_process');
 
 try {
-    let existing;
+    let existing = '';
     try {
-        existing = fs.readFileSync('.package.json.md5');
+        existing = fs.readFileSync('.package.json.md5') || '';
     } catch (ex) {
         // don't care
     }
     const actual = execSync(process.platform === 'darwin' ? 'md5 -q package.json' : 'md5sum package.json');
     if (existing.toString() !== actual.toString()) {
         console.log('package.json has changed, running npm install.');
-        execSync('npm install');
+        const ret = spawnSync('npm', ['install']);
+        console.log(ret.stderr && ret.stderr.toString());
+        console.log(ret.stdout && ret.stdout.toString());
     } else {
         console.log('package.json has not changed, skipping npm install.');
     }
--- scripts/store_package_json_hash.js
+++ scripts/store_package_json_hash.js
@@ -2,8 +2,11 @@ if (process.env.CI) return;
 
 const { execSync } = require('child_process');
 
+console.log('Storing package.json hash');
 if (process.platform === 'darwin') {
+    console.log('Using md5 command under osx.');
     execSync('md5 -q package.json > .package.json.md5');
 } else {
+    console.log('Using md5sum command under linux.');
     execSync('md5sum package.json > .package.json.md5');
 }
